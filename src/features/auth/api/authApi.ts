39c272d7f982606221329471c815301fb6ce0d7d
Saveliy Zhadan <savely.jadan@gmail.com>
2026-02-21 15:23:49 +0700

    feat(auth): add login endpoint

---
+++ src/features/auth/api/authApi.ts
@@ -0,0 +1,20 @@
+import { mapAuthResponseToUser } from '@/entities/user'
+import type { User } from '@/entities/user/model/types'
+import { baseApi } from '@/shared/api'
+import { type RequestLoginBody } from './types'
+
+const userApi = baseApi.injectEndpoints({
+    endpoints: (build) => ({
+        login: build.mutation<User, RequestLoginBody>({
+            query: (body) => ({
+                url: 'auth/login',
+                method: 'POST',
+                body,
+                credentials: 'include',
+            }),
+            transformResponse: mapAuthResponseToUser,
+        }),
+    }),
+})
+
+export const { useLoginMutation } = userApi
