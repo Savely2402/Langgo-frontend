a068b13edf7cca00e1092bb6fd82d8d015160b5c
Saveliy Zhadan <savely.jadan@gmail.com>
2026-02-26 05:22:14 +0700

    feat(auth): add auth card widget

---
+++ src/widgets/auth-card/ui/AuthCard.tsx
@@ -0,0 +1,56 @@
+import type React from 'react'
+import GoogleSvg from '@/shared/assets/icons/google.svg?react'
+import {
+    Button,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+    Separator,
+    Icon,
+    CardFooter,
+} from '@/shared/ui'
+
+interface AuthFormProps {
+    title: string
+    description: string
+    children: React.ReactNode
+    footer: React.ReactNode
+}
+
+export const AuthCard = ({
+    title,
+    description,
+    children,
+    footer,
+}: AuthFormProps) => {
+    return (
+        <>
+            <Card className="w-full max-w-[420px]">
+                <CardHeader className="flex flex-col items-center">
+                    <CardTitle>{title}</CardTitle>
+                    <CardDescription>{description}</CardDescription>
+                </CardHeader>
+                <CardContent className="w-full">
+                    <Button
+                        className="w-full bg-white h-10.5 text-muted-foreground"
+                        variant="outline"
+                    >
+                        <Icon Svg={GoogleSvg} />
+                        Sign in with google
+                    </Button>
+                    <div className="flex w-full items-center gap-2 my-6">
+                        <Separator className="flex-1" />
+                        <span>or continue with</span>
+                        <Separator className="flex-1" />
+                    </div>
+                    {children}
+                </CardContent>
+                <CardFooter className="flex justify-center">
+                    {footer}
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
