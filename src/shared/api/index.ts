d0ae92ac1fcb50a5151bc4d0a33ed41ca333242c
Saveliy Zhadan <savely.jadan@gmail.com>
2026-02-26 05:25:21 +0700

    chore(api): reexport typeguards and error utitlites

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,3 +1,5 @@
 export { baseApi } from './baseApi'
 export { AUTH_TAG } from './tags'
-export { sendBadAuthorization } from '../mocks/mswHelpers'
+export { getErrorMessage } from './errorUtils'
+export { isRtkQueryError } from './typeGuards'
+export { REDUCER_PATH } from './constants'
