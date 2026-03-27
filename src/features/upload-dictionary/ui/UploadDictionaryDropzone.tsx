a8cd6dfdee7f2c2fc019fbd97c1364e77bf26bd3
Saveliy Zhadan <savely.jadan@gmail.com>
2026-03-27 15:31:22 +0700

    fix: wrap upload dictionary dropzone in react memo to avoid rerenders

--- src/features/upload-dictionary/ui/UploadDictionaryDropzone.tsx
+++ src/features/upload-dictionary/ui/UploadDictionaryDropzone.tsx
@@ -1,69 +1,70 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import type { BaseDictionary } from '@/entities/dictionary'
 import { cn } from '@/shared/lib/classNames'
-import {
-    useUploadDictionary,
-    type UploadStatus,
-} from '../model/useUploadDictionary'
+import { useUploadDictionary } from '../model/useUploadDictionary'
 import { UploadErrorsAlert } from './UploadErrorsAlert'
 import { UploadIdle } from './UploadIdle'
 import { UploadLoading } from './UploadLoading'
 import { UploadSuccess } from './UploadSuccess'
+import type { UploadStatus } from '../model/types'
 
 interface UploadDictionaryDropzoneProps {
     onSuccess?: (dictionaryId: BaseDictionary['id']) => void
     onStatusChange?: (status: UploadStatus) => void
 }
 
-export const UploadDictionaryDropzone = ({
-    onSuccess,
-    onStatusChange,
-}: UploadDictionaryDropzoneProps) => {
-    const {
-        getRootProps,
-        getInputProps,
-        isDragActive,
-        status,
-        dictionaryData,
-        handleCancel,
-        handleRemove,
-        errors,
-    } = useUploadDictionary({ onSuccess })
+export const UploadDictionaryDropzone = React.memo(
+    ({
+        onSuccess,
+        onStatusChange,
+        ...props
+    }: UploadDictionaryDropzoneProps) => {
+        const {
+            getRootProps,
+            getInputProps,
+            isDragActive,
+            status,
+            dictionaryData,
+            handleCancel,
+            handleRemove,
+            errors,
+        } = useUploadDictionary({ onSuccess })
 
-    useEffect(() => {
-        if (onStatusChange) {
-            onStatusChange(status)
-        }
-    }, [status, onStatusChange])
+        useEffect(() => {
+            if (onStatusChange) {
+                onStatusChange(status)
+            }
+        }, [status, onStatusChange])
 
-    return (
-        <div className="flex w-full max-w-[558px] flex-col">
-            {status === 'loading' && (
-                <UploadLoading handleCancel={handleCancel} />
-            )}
+        return (
+            <div className="flex w-full max-w-[558px] flex-col" {...props}>
+                {status === 'loading' && (
+                    <UploadLoading handleCancel={handleCancel} />
+                )}
 
-            {status === 'success' && dictionaryData && (
-                <UploadSuccess
-                    dictionaryName={dictionaryData.name}
-                    langFrom={dictionaryData.languagePair.langFrom}
-                    langTo={dictionaryData.languagePair.langTo}
-                    wordsAmount={dictionaryData.wordsAmount}
-                    onRemove={handleRemove}
-                />
-            )}
-
-            {(status === 'idle' || status === 'error') && (
-                <div className="flex w-full flex-col gap-4">
-                    <UploadIdle
-                        getRootProps={getRootProps}
-                        getInputProps={getInputProps}
-                        className={cn(
-                            isDragActive && 'border-primary bg-white',
-                        )}
+                {status === 'success' && dictionaryData && (
+                    <UploadSuccess
+                        dictionaryName={dictionaryData.name}
+                        langFrom={dictionaryData.languagePair.langFrom}
+                        langTo={dictionaryData.languagePair.langTo}
+                        wordsAmount={dictionaryData.wordsAmount}
+                        onRemove={handleRemove}
                     />
-                    <UploadErrorsAlert errors={errors} />
-                </div>
-            )}
-        </div>
-    )
-}
+                )}
+
+                {(status === 'idle' || status === 'error') && (
+                    <div className="flex w-full flex-col gap-4">
+                        <UploadIdle
+                            getRootProps={getRootProps}
+                            getInputProps={getInputProps}
+                            className={cn(
+                                isDragActive && 'border-primary bg-white',
+                            )}
+                        />
+                        <UploadErrorsAlert errors={errors} />
+                    </div>
+                )}
+            </div>
+        )
+    },
+)
